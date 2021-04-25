2b9b2dc0b431f1d00f4952c7b9b56aa378cc379e
Ta-Ye <cty1272@naver.com>
2021-04-25 17:08:10 +0900

    study 1

---
+++ server/basic-test/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,15 @@
+package com.sp.fc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+	
+	@DisplayName("1. Å×½ºÆ® ")
+	@Test
+	void test_1() {
+		assertEquals("test", "test");
+	}
+}
